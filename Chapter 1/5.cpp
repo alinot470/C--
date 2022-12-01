babf71ed82cfa8adefe85e95205b0476d2acb6f1
Ali <alinot470@gmail.com>
2022-12-01 08:32:55 +0500

    update

---
+++ Chapter 1/5.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#include <cstdio>
+using namespace std;
+int main()
+{
+  int  x;
+  long l;
+  char ch;
+  float y;
+  double z;
+
+  cin >> x;
+  cin >> l;
+  cin >> ch;
+  cin >> y;
+  cin >> z;
+
+  cout << x << endl;
+  cout << l << endl; 
+  cout << ch << endl;
+  cout << fixed<<setprecision(3)<<y<<endl ;
+  cout << fixed<<setprecision(9)<< z << endl;
+
+  return 0;
+
+}
