65d2b36268056e4de425db92239dd1bf1a660655
Ali <alinot470@gmail.com>
2023-01-21 21:31:13 +0500

    update

---
+++ Chapter 2/12.cpp
@@ -0,0 +1,19 @@
+// Many financial experts advise property owners to insure
+// their homes or buildings for at least 80 percent of the
+// amount it would cost to replace the structure. Write a
+// program that asks the user to enter the replacement cost of
+// a building and then displays the minimum amount of
+// insurance that should be purchased for the property?
+#include <iostream>
+using namespace std;
+int main ()
+{
+  float replacement_cost , insurance_percent = .80 , minimum_amount_property;
+  cout << "Enter the replacment cost :";
+  cin>> replacement_cost;
+  minimum_amount_property = replacement_cost * insurance_percent;
+  cout << minimum_amount_property;
+
+  
+
+}
